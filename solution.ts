1e52595ba68a0dcd703944c62f7439134e4f9424
tanzia mourin <tanziamourin68@gmail.com>
2025-11-16 21:51:56 +0600

    first commit

---
+++ solution.ts
@@ -0,0 +1,25 @@
+// problem -- 1
+
+function formatValue (value: string| number| boolean) :  string | number| boolean {
+    if(typeof value === "string"){
+        return value.toUpperCase();
+    }
+    if(typeof  value === "number"){
+        return value * 10;
+    }
+    if(typeof value === "boolean"){
+        return  !value;
+    }
+    return value
+
+}
+
+// problem -- 2
+
+
+// problem -- 3
+// problem -- 4
+// problem -- 5
+// problem -- 6
+// problem -- 7
+// problem -- 8
